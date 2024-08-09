ebc6ca37b26622b8fd939cce96305a267b769e89
Paulo RFZ <paulorfz1122@gmail.com>
2024-08-09 00:30:56 -0300

    Testando

---
+++ SKZTesteSql/Program.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+
+namespace DataBaseConnectSKZ
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            var produtos = GetProduct();
+
+            Console.WriteLine("Hello World!");
+        }
+
+        private static IEnumerable<Product> GetProduct()
+        {
+            using(var connection = new SqlConnection("Server=DESKTOP-JRR030Q\SQLEXPRESS;Database=SKZTeste;Trusted_Connection=True;"))
+            {
+                return connection.Query<Product>("Select*from product");
+            }
+        }
+    }
+}
